99a51c7345669a25564f5aa1b763286e50a68b97
Smit Rabari <smitrabari18@gmail.com>
2026-07-02 21:49:02 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (83.65%) - LeetSync

---
+++ 7-reverse-integer/reverse-integer.cpp
@@ -0,0 +1,28 @@
+#include<cmath>
+class Solution {
+public:
+    long reverse(long x) 
+    {
+        long rev = 0;
+        long org = x;
+        if(x<0)
+        {
+            x = -x;
+        }
+        while(x>0)
+        {
+            long lastdigit = x % 10;
+            rev = lastdigit + rev*10;
+            x = x / 10;
+        }
+        if(org<0)
+        {
+            rev = -rev;
+        }
+        if(rev<= -pow(2,31) || rev>=pow(2,31)-1)
+        {
+            rev = 0;
+        }
+        return rev;
+    }
+};
