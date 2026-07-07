5a15e2e1424a0a0d0d80430e8505c80c73d7344f
Smit Rabari <smitrabari18@gmail.com>
2026-07-07 20:25:22 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (41.06%) - LeetSync

--- 2608-count-the-digits-that-divide-a-number/count-the-digits-that-divide-a-number.cpp
+++ 2608-count-the-digits-that-divide-a-number/count-the-digits-that-divide-a-number.cpp
@@ -3,18 +3,25 @@ class Solution
 public:
     int countDigits(int num) 
     {
-        int org = num;
+        int org = num; 
+        // Making the Org Variable so that after we pick up the
+        // last digit the number remains the same
         int count=0;
-        while(num > 0) 
+        // always count variable should be declare 0
+        while(num > 0)  
+        // condition for last digit
         {
             int digit = num % 10;
-            
-            if( digit!=0 && org%digit==0)
+            // taken the last digit
+            if( digit!=0 && org%digit==0 )
+            // here the digit should not be zero and 
+            // that should be divisible by the orginal number
             {
                 count++;
             }
 
             num = num / 10;
+            // here we have new value
         }
         return count;
     }
