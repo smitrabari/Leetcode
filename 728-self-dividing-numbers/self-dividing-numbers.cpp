2b2c3458fa97796c3294103d8edd5efabb53a6f4
Smit Rabari <smitrabari18@gmail.com>
2026-07-07 23:05:11 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (27.65%) - LeetSync

--- 728-self-dividing-numbers/self-dividing-numbers.cpp
+++ 728-self-dividing-numbers/self-dividing-numbers.cpp
@@ -2,12 +2,16 @@ class Solution {
 public:
     vector<int> selfDividingNumbers(int left, int right) {
         vector<int> ls;
+        // created a vector to store the self dividing numbers
         for(int i = left ; i <= right ; i++)
+        // for loop for starting the range from left
         {
             int temp = i;
+            // a temp. variable of i
             bool isdivisiable = true;
 
             while(temp>0)
+            // lastdigit while loop
             {
                 int lastdigit = temp % 10;
                 
@@ -21,6 +25,7 @@ public:
             if(isdivisiable)
             {
                 ls.push_back(i);
+                // adding values to the vector
             }
         }
         return ls;
